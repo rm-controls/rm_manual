db1b4891a3bb47542c5db7d3100a99cb2908d36c
astro <1015926336@qq.com>
2021-05-17 15:01:55 +0800

    Add wait for controller manager existence before load controllers

--- src/controller_manager.cpp
+++ src/controller_manager.cpp
@@ -14,35 +14,44 @@ ControllerManager::ControllerManager(ros::NodeHandle &node_handle) : nh_(node_ha
   nh_.getParam("load_controllers", *controllers_);
 }
 void ControllerManager::loadAllControllers() {
+  load_controllers_client_.waitForExistence(ros::Duration(1.5));
   for (int i = 0; i < controllers_->size(); ++i) {
-
     load_controller_srv_.request.name.clear();
     load_controller_srv_.request.name.append(controllers_[0][i]);
-    while (!load_controllers_client_.call(load_controller_srv_)) {
-      ros::Duration(0.05).sleep();//load controller need some time
-    }
-    std::cout << "loaded " << controllers_[0][i] << std::endl;
+    if (load_controllers_client_.call(load_controller_srv_))
+      std::cout << "loaded " << controllers_[0][i] << std::endl;
+    else
+      std::cout << "fail to load " << controllers_[0][i] << std::endl;
   }
 }
 void ControllerManager::startAllControllers() {
   for (int i = 0; i < controllers_->size(); ++i) {
     switch_controller_srv_.request.start_controllers.push_back(controllers_[0][i]);
-    std::cout << "statrt" << (std::string) controllers_[0][i] << std::endl;
+
   }
   switch_controller_srv_.request.stop_controllers.clear();
   switch_controller_srv_.request.strictness = switch_controller_srv_.request.BEST_EFFORT;
   switch_controller_srv_.request.start_asap = false;
   switch_controller_srv_.request.timeout = 0.5;
-  switch_controller_client_.call(switch_controller_srv_);
+  if (switch_controller_client_.call(switch_controller_srv_)) {
+    std::cout << "start controllers" << std::endl;
+  } else {
+    std::cout << "can not start controllers" << std::endl;
+  }
 }
 void ControllerManager::stopAllControllers() {
   switch_controller_srv_.request.start_controllers.clear();
   for (int i = 0; i < controllers_->size(); ++i) {
     switch_controller_srv_.request.stop_controllers.push_back(controllers_[0][i]);
+
   }
   switch_controller_srv_.request.strictness = switch_controller_srv_.request.BEST_EFFORT;
   switch_controller_srv_.request.start_asap = false;
   switch_controller_srv_.request.timeout = 0.5;
-  switch_controller_client_.call(switch_controller_srv_);
+  if (switch_controller_client_.call(switch_controller_srv_)) {
+    std::cout << "stop controllers" << std::endl;
+  } else {
+    std::cout << "can not stop controllers" << std::endl;
+  }
 }
 
