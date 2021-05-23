26facc5aaaa171c237786fce5fa04f1d72892811
qiayuan <liaoqiayuan@gmail.com>
2021-05-24 00:29:06 +0800

    Add EngineerManual

---
+++ include/rm_manual/engineer_manual.h
@@ -0,0 +1,18 @@
+//
+// Created by qiayuan on 5/23/21.
+//
+
+#ifndef RM_MANUAL_ENGINEER_MANUAL_H_
+#define RM_MANUAL_ENGINEER_MANUAL_H_
+
+#include "rm_manual/chassis_gimbal_manual.h"
+
+namespace rm_manual {
+class EngineerManual : public ChassisGimbalManual {
+ public:
+ private:
+  VelCommandSender vel_command_sender_;
+};
+
+}
+#endif //RM_MANUAL_ENGINEER_MANUAL_H_
