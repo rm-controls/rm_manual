0f5793bd68df95c050c7bcd332c76b5d305af47c
chenzheng <1047979458@11.com>
2021-07-18 16:24:14 +0800

    Change referee data interface to reference

--- include/rm_manual/chassis_gimbal_manual.h
+++ include/rm_manual/chassis_gimbal_manual.h
@@ -11,11 +11,11 @@ class ChassisGimbalManual : public ManualBase {
  public:
   explicit ChassisGimbalManual(ros::NodeHandle &nh) : ManualBase(nh) {
     ros::NodeHandle chassis_nh(nh, "chassis");
-    chassis_cmd_sender_ = new rm_common::ChassisCommandSender(chassis_nh);
+    chassis_cmd_sender_ = new rm_common::ChassisCommandSender(chassis_nh, data_.referee_.referee_data_);
     ros::NodeHandle vel_nh(nh, "vel");
     vel_cmd_sender_ = new rm_common::Vel2DCommandSender(vel_nh);
     ros::NodeHandle gimbal_nh(nh, "gimbal");
-    gimbal_cmd_sender_ = new rm_common::GimbalCommandSender(gimbal_nh);
+    gimbal_cmd_sender_ = new rm_common::GimbalCommandSender(gimbal_nh, data_.referee_.referee_data_);
     ui_chassis_ = new UiChassis(&data_.referee_);
     ui_gimbal_ = new UiGimbal(&data_.referee_);
     ui_capacitor_ = new UiCapacitor(&data_.referee_);
@@ -33,7 +33,6 @@ class ChassisGimbalManual : public ManualBase {
   }
   void updateRc() override {
     ManualBase::updateRc();
-    chassis_cmd_sender_->updateLimit(data_.referee_.referee_data_);
     if (std::abs(data_.dbus_data_.wheel) > 0.01) {
       vel_cmd_sender_->setAngularZVel(data_.dbus_data_.wheel);
       chassis_cmd_sender_->setMode(rm_msgs::ChassisCmd::GYRO);
@@ -45,7 +44,6 @@ class ChassisGimbalManual : public ManualBase {
   }
   void updatePc() override {
     ManualBase::updatePc();
-    chassis_cmd_sender_->updateLimit(data_.referee_.referee_data_);
     gimbal_cmd_sender_->setRate(-data_.dbus_data_.m_x, data_.dbus_data_.m_y);
   }
   void rightSwitchDown(ros::Duration time) override {
