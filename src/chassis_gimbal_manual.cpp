55154aa84fc02dd3bf7a6916901b95c0bd185c2e
chenzheng <1047979458@11.com>
2021-07-29 09:40:29 +0800

    Delete some debug info

--- src/chassis_gimbal_manual.cpp
+++ src/chassis_gimbal_manual.cpp
@@ -83,17 +83,12 @@ void ChassisGimbalManual::drawUi(const ros::Time &time) {
                         && vel_cmd_sender_->getMsg()->angular.z != 0.);
   if (data_.dbus_data_.s_l == rm_msgs::DbusData::MID && data_.dbus_data_.s_r == rm_msgs::DbusData::UP){
     chassis_cmd_sender_->power_limit_->updateState(rm_common::PowerLimit::TEST);
-    ROS_INFO("test");
     trigger_change_ui_->update("chassis",
                                chassis_cmd_sender_->getMsg()->mode,
                                false,
                                1,
                                false);
-  }
-
-
-  else{
-    ROS_INFO("bust or charge");
+  } else{
     trigger_change_ui_->update("chassis",
                                chassis_cmd_sender_->getMsg()->mode,
                                (chassis_cmd_sender_->power_limit_->getState() == rm_common::PowerLimit::BURST),
