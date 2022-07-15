19454f67bfc0f378b100d7d9c7742616762c0e64
Edwinlinks <1026085014@qq.com>
2022-07-15 20:39:43 +0800

    Update new manual for dart.

---
+++ include/rm_manual/dart_manual.h
@@ -0,0 +1,48 @@
+//
+// Created by luotinkai on 2022/7/15.
+//
+
+#ifndef SRC_DART_MANUAL_H
+#define SRC_DART_MANUAL_H
+
+#include "rm_manual/chassis_gimbal_manual.h"
+#include <actionlib/client/simple_action_client.h>
+#include <rm_common/decision/calibration_queue.h>
+#include <std_msgs/Float64.h>
+#include <std_srvs/Empty.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <utility>
+
+namespace rm_manual
+{
+class DartManual : public ManualBase
+{
+public:
+  explicit DartManual(ros::NodeHandle& nh);
+
+protected:
+  void sendCommand(const ros::Time& time) override;
+  void run() override;
+  void checkReferee() override;
+  void remoteControlTurnOn() override;
+  void leftSwitchUpFall();
+  void leftSwitchDownRise() override;
+  void leftSwitchMidRise() override;
+  void leftSwitchUpRise() override;
+  void updateRc() override;
+  void updatePc() override;
+  void Move(int index, double ch_l, rm_common::JointPointCommandSender* joint);
+  void recordPosition();
+
+  rm_common::JointPointCommandSender *trigger_sender_, *friction_left_sender_, *friction_right_sender_;
+  rm_common::JointPointCommandSender *pitch_sender_, *yaw_sender_;
+  rm_common::CalibrationQueue *trigger_calibration_, *gimbal_calibration_;
+  double pitch_post_{}, pitch_base_{}, yaw_post_{}, yaw_base_{};
+  double qd_, forward_vel_;
+  bool set_position_{ true };
+
+  InputEvent chassis_power_on_event_, gimbal_power_on_event_;
+};
+}  // namespace rm_manual
+
+#endif  // SRC_DART_MANUAL_H
---
+++ src/dart_manual.cpp
@@ -0,0 +1,146 @@
+//
+// Created by luotinkai on 2022/7/15.
+//
+
+#include "rm_manual/dart_manual.h"
+
+namespace rm_manual
+{
+DartManual::DartManual(ros::NodeHandle& nh) : ManualBase(nh)
+{
+  ros::NodeHandle nh_yaw = ros::NodeHandle(nh, "yaw");
+  ros::NodeHandle nh_left_pitch = ros::NodeHandle(nh, "left_pitch");
+  yaw_sender_ = new rm_common::JointPointCommandSender(nh_yaw, data_.joint_state_);
+  pitch_sender_ = new rm_common::JointPointCommandSender(nh_left_pitch, data_.joint_state_);
+
+  ros::NodeHandle nh_trigger = ros::NodeHandle(nh, "trigger");
+  ros::NodeHandle nh_friction_left = ros::NodeHandle(nh, "friction_left");
+  ros::NodeHandle nh_friction_right = ros::NodeHandle(nh, "friction_right");
+  qd_ = getParam(nh_friction_left, "qd_", 0.);
+  forward_vel_ = getParam(nh_trigger, "forward_vel", 0.);
+  trigger_sender_ = new rm_common::JointPointCommandSender(nh_trigger, data_.joint_state_);
+  friction_left_sender_ = new rm_common::JointPointCommandSender(nh_friction_left, data_.joint_state_);
+  friction_right_sender_ = new rm_common::JointPointCommandSender(nh_friction_right, data_.joint_state_);
+  XmlRpc::XmlRpcValue trigger_rpc_value, gimbal_rpc_value;
+  nh.getParam("trigger_calibration", trigger_rpc_value);
+  trigger_calibration_ = new rm_common::CalibrationQueue(trigger_rpc_value, nh, controller_manager_);
+  nh.getParam("gimbal_calibration", gimbal_rpc_value);
+  gimbal_calibration_ = new rm_common::CalibrationQueue(gimbal_rpc_value, nh, controller_manager_);
+
+  left_switch_up_event_.setEdge(boost::bind(&DartManual::leftSwitchUpRise, this),
+                                boost::bind(&DartManual::leftSwitchUpFall, this));
+  chassis_power_on_event_.setRising(boost::bind(&DartManual::chassisOutputOn, this));
+  gimbal_power_on_event_.setRising(boost::bind(&DartManual::gimbalOutputOn, this));
+}
+
+void DartManual::run()
+{
+  ManualBase::run();
+  trigger_calibration_->update(ros::Time::now());
+  gimbal_calibration_->update(ros::Time::now());
+}
+
+void DartManual::sendCommand(const ros::Time& time)
+{
+  friction_left_sender_->sendCommand(time);
+  friction_right_sender_->sendCommand(time);
+  trigger_sender_->sendCommand(time);
+  pitch_sender_->sendCommand(time);
+  yaw_sender_->sendCommand(time);
+}
+
+void DartManual::updateRc()
+{
+  ManualBase::updateRc();
+  Move(pitch_sender_->findJoint(), data_.dbus_data_.ch_r_y, pitch_sender_);
+  Move(yaw_sender_->findJoint(), data_.dbus_data_.ch_l_x, yaw_sender_);
+}
+
+void DartManual::updatePc()
+{
+  ManualBase::updatePc();
+  recordPosition();
+  if (data_.dbus_data_.ch_l_y == 1)
+  {
+    pitch_sender_->setPoint(pitch_post_);
+    yaw_sender_->setPoint(yaw_post_);
+  }
+  if (data_.dbus_data_.ch_l_y == -1)
+  {
+    pitch_sender_->setPoint(pitch_base_);
+    yaw_sender_->setPoint(yaw_base_);
+  }
+}
+
+void DartManual::checkReferee()
+{
+  ManualBase::checkReferee();
+}
+
+void DartManual::remoteControlTurnOn()
+{
+  ManualBase::remoteControlTurnOn();
+  gimbal_calibration_->reset();
+}
+
+void DartManual::leftSwitchDownRise()
+{
+  ManualBase::leftSwitchDownRise();
+  friction_right_sender_->setPoint(0.);
+  friction_left_sender_->setPoint(0.);
+  trigger_calibration_->reset();
+}
+
+void DartManual::leftSwitchMidRise()
+{
+  ManualBase::leftSwitchMidRise();
+  ROS_INFO("on");
+  friction_right_sender_->setPoint(qd_);
+  friction_left_sender_->setPoint(qd_);
+}
+
+void DartManual::leftSwitchUpRise()
+{
+  ManualBase::leftSwitchUpRise();
+  trigger_sender_->setPoint(forward_vel_);
+}
+
+void DartManual::leftSwitchUpFall()
+{
+  trigger_sender_->setPoint(0.);
+}
+
+void DartManual::Move(int index, double ch_l, rm_common::JointPointCommandSender* joint)
+{
+  if (!data_.joint_state_.position.empty())
+  {
+    double position = data_.joint_state_.position[index];
+    if (ch_l != 0)
+    {
+      joint->setPoint(position + ch_l * 0.04);
+      set_position_ = true;
+    }
+    if (ch_l == 0 && set_position_)
+    {
+      joint->setPoint(data_.joint_state_.position[index]);
+      set_position_ = false;
+    }
+  }
+}
+
+void DartManual::recordPosition()
+{
+  if (data_.dbus_data_.ch_r_y == 1)
+  {
+    pitch_post_ = data_.joint_state_.position[1];
+    yaw_post_ = data_.joint_state_.position[5];
+    ROS_INFO("recorded outpost position");
+  }
+  if (data_.dbus_data_.ch_r_y == -1)
+  {
+    pitch_base_ = data_.joint_state_.position[1];
+    yaw_base_ = data_.joint_state_.position[5];
+    ROS_INFO("recorded base position");
+  }
+}
+}  // namespace rm_manual
