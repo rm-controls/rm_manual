a582fd79ed176e744493a32f1ab6da6c85a13ede
lsy <2028238109@qq.com>
2022-11-24 11:57:50 +0800

    update from nuc 11.24 11h57min.

--- src/engineer_manual.cpp
+++ src/engineer_manual.cpp
@@ -136,10 +136,7 @@ void EngineerManual::sendCommand(const ros::Time& time)
     chassis_cmd_sender_->sendCommand(time);
     vel_cmd_sender_->sendCommand(time);
     if (servo_mode_ == SERVO)
-    //      servo_command_sender_->sendCommand(time);
-    {
-      ;
-    }
+      servo_command_sender_->sendCommand(time);
   }
   if (gimbal_mode_ == RATE)
   {
