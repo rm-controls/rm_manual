1e4e4fc0d7b3456e19116d7a2d3bf63984e984cc
d0h0s <3328432363@qq.com>
2023-02-20 11:35:15 +0800

    Fixed some details.

--- src/manual_base.cpp
+++ src/manual_base.cpp
@@ -152,14 +152,4 @@ void ManualBase::robotDie()
   }
 }
 
-// void ManualBase::rightSwitchDownRise()
-//{
-//   rightSwitchDownRiseState();
-// }
-//
-// void ManualBase::rightSwitchUpRise()
-//{
-//   rightSwitchUpRiseState();
-// }
-
 }  // namespace rm_manual
