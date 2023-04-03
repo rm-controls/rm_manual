b8b598761e2783291c7cd9f201dbdf2565458043
LSY <2028238109@qq.com>
2023-04-03 12:22:16 +0800

    Fix code style.

--- include/rm_manual/engineer_manual.h
+++ include/rm_manual/engineer_manual.h
@@ -22,6 +22,21 @@ namespace rm_manual
 class EngineerManual : public ChassisGimbalManual
 {
 public:
+  enum
+  {
+    MANUAL,
+    MIDDLEWARE
+  };
+  enum
+  {
+    SERVO,
+    JOINT
+  };
+  enum
+  {
+    RATE,
+    DIRECT
+  };
   EngineerManual(ros::NodeHandle& nh, ros::NodeHandle& nh_referee);
   void run() override;
 
@@ -109,43 +124,28 @@ private:
                      std::string joint_temperature, std::string gripper_state);
   void sendUi(std::string step_name, std::string reversal_state, std::string drag_state, uint8_t stone_num,
               std::string joint_temperature, std::string gripper_state);
-  enum
-  {
-    MANUAL,
-    MIDDLEWARE
-  };
-  enum
-  {
-    SERVO,
-    JOINT
-  };
-  enum
-  {
-    RATE,
-    DIRECT
-  };
 
-  int state_;
-  bool change_flag_ = 1, is_exchange_, target_shape_;
   bool reversal_motion_{};
-  rm_msgs::StepQueueState step_queue_state_;
-  rm_msgs::EngineerUi engineer_ui_;
-  ros::Publisher ui_send_;
+  bool change_flag_ = 1, is_exchange_, target_shape_;
+  int state_, operating_mode_{}, servo_mode_{}, gimbal_mode_{}, stone_num_{}, stone_num_last_{}, max_temperature_{};
   double angular_z_scale_{};
   std::string prefix_, root_, step_name_last_, reversal_state_last_, drag_state_, drag_state_last_,
       max_temperature_joint_, joint_temperature_, joint_temperature_last_, reversal_state_, gripper_state_,
       gripper_state_last_;
-  int operating_mode_{}, servo_mode_{}, gimbal_mode_{}, stone_num_{}, stone_num_last_{}, max_temperature_{};
-  std::map<std::string, int> prefix_list_, root_list_;
+
   ros::Time last_time_;
+  ros::Publisher ui_send_;
   ros::Subscriber reversal_vision_sub_;
   ros::Publisher step_queue_state_pub_;
   actionlib::SimpleActionClient<rm_msgs::EngineerAction> action_client_;
-  rm_common::CalibrationQueue *power_on_calibration_{}, *arm_calibration_{};
+
+  rm_msgs::EngineerUi engineer_ui_;
+  rm_msgs::StepQueueState step_queue_state_;
   rm_common::Vel3DCommandSender* servo_command_sender_;
-  rm_common::ServiceCallerBase<std_srvs::Empty>* servo_reset_caller_;
   rm_common::MultiDofCommandSender* reversal_command_sender_;
+  rm_common::ServiceCallerBase<std_srvs::Empty>* servo_reset_caller_;
   rm_common::JointPositionBinaryCommandSender* drag_command_sender_;
+  rm_common::CalibrationQueue *power_on_calibration_{}, *arm_calibration_{};
   InputEvent left_switch_up_event_, left_switch_down_event_, ctrl_q_event_, ctrl_a_event_, ctrl_z_event_, ctrl_w_event_,
       ctrl_s_event_, ctrl_x_event_, ctrl_e_event_, ctrl_d_event_, ctrl_c_event_, ctrl_b_event_, ctrl_v_event_, z_event_,
       q_event_, e_event_, x_event_, c_event_, v_event_, b_event_, f_event_, shift_z_event_, shift_x_event_,
