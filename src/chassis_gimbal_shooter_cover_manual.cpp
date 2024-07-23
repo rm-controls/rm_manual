957af8e2e35c90835e8828612cb0d2c148e22cbe
liyixin135 <421159734@qq.com>
2024-07-23 20:44:38 +0800

    Update for clean.

--- src/chassis_gimbal_shooter_cover_manual.cpp
+++ src/chassis_gimbal_shooter_cover_manual.cpp
@@ -213,12 +213,11 @@ void ChassisGimbalShooterCoverManual::ctrlZPress()
 
 void ChassisGimbalShooterCoverManual::mouseRightPress()
 {
+  ChassisGimbalShooterManual::mouseRightPress();
   if (is_auto_)
     sentryMode();
   else if (!mouse_left_event_.getState() && shooter_cmd_sender_->getMsg()->mode == rm_msgs::ShootCmd::PUSH)
     shooter_cmd_sender_->setMode(rm_msgs::ShootCmd::READY);
-  else
-    ChassisGimbalShooterManual::mouseRightPress();
 }
 
 void ChassisGimbalShooterCoverManual::sentryMode()
@@ -236,9 +235,8 @@ void ChassisGimbalShooterCoverManual::sentryMode()
   if (track_data_.id == 0)
   {
     gimbal_cmd_sender_->setMode(rm_msgs::GimbalCmd::TRAJ);
-    double traj_yaw, traj_pitch;
-    traj_yaw = M_PI * count_ / 900;
-    traj_pitch = 0.15 * sin(2 * M_PI * count_ / 900) + 0.2;
+    double traj_yaw = M_PI * count_ / 900;
+    double traj_pitch = 0.15 * sin(2 * M_PI * count_ / 900) + 0.2;
     count_ = (count_ + 1) % 900;
     gimbal_cmd_sender_->setYawAndPitchTraj(traj_yaw, traj_pitch);
     shooter_cmd_sender_->setMode(rm_msgs::ShootCmd::READY);
