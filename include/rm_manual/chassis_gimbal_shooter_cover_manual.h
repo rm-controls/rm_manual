3c338e2dcdd19b9b0a6dfb68b1a806116b60f52c
liyixin135 <421159734@qq.com>
2024-07-31 16:20:40 +0800

    Add sentry mode when pressing ctrlR.

--- include/rm_manual/chassis_gimbal_shooter_cover_manual.h
+++ include/rm_manual/chassis_gimbal_shooter_cover_manual.h
@@ -33,6 +33,8 @@ protected:
   void zRelease();
   void wPress() override;
   void wPressing() override;
+  void ctrlRPressing();
+  void ctrlRRelease() override;
 
   virtual void ctrlZPress();
   virtual void ctrlZRelease()
@@ -50,5 +52,6 @@ protected:
   ros::Time last_switch_time_;
   bool supply_ = false;
   bool cover_close_ = true;
+  int count_{};
 };
 }  // namespace rm_manual
