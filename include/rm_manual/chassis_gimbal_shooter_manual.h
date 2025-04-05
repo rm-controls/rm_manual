ea09751e89e4b007136798f6f311ef3b9628569e
chen-gr <725393026@qq.com>
2025-04-05 20:05:41 +0800

    Change close gyro shortcut to Q key.

--- include/rm_manual/chassis_gimbal_shooter_manual.h
+++ include/rm_manual/chassis_gimbal_shooter_manual.h
@@ -93,7 +93,7 @@ protected:
   virtual void shiftPress();
   virtual void shiftRelease();
   void rPress();
-  void qPress()
+  virtual void qPress()
   {
     shooter_cmd_sender_->setShootFrequency(rm_common::HeatLimit::BURST);
   }
